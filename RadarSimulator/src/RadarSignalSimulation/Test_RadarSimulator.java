06d0f47b4a836f5b663313d1462dcea3a12b7b51
ismontes <imontes2694@gmail.com>
2026-06-17 09:38:27 -0700

    Debugging fix to fit the [tolerance] variable

--- RadarSimulator/src/RadarSignalSimulation/Test_RadarSimulator.java
+++ RadarSimulator/src/RadarSignalSimulation/Test_RadarSimulator.java
@@ -7,11 +7,11 @@ public class Test_RadarSimulator
 	{
 		RadarSimulator test1 = new RadarSimulator();
 		System.out.println("Test 1: " + test1.detectionRate(
-										100, 500, 15, 1.0, 2.5));
+										100, 500, 15, 1.0, 2.5, 1));
 	
 		RadarSimulator test2 = new RadarSimulator();
 		System.out.println("Test 2: " + test2.detectionRate(
-										100, 500, 3, 4.0, 10.0));
+										100, 500, 3, 4.0, 10.0, 2));
 	}
 
 }
